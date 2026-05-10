2090c1b0279db0a8e4e16aff53402ba19d882eef
srishti12-code <menonsrishti.12@gmail.com>
2026-05-10 13:52:51 +0530

    Update Solarapp.py

--- Solarapp.py
+++ Solarapp.py
@@ -8,7 +8,7 @@ import json
 # Configuration
 API_KEY = "AIzaSyA-mPQ8TUx9vr3-BwJRPbbicxOPZmGMp7w"
 genai.configure(api_key=API_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-1.5-flash-latest')
 
 st.set_page_config(page_title="Solar Bill Automator", page_icon="☀️")
 
